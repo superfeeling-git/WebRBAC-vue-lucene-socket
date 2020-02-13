fa1f4fc70312cf62d48964e3a6c633d39b04fae8
superfeeling-git <58456025+superfeeling-git@users.noreply.github.com>
2020-02-13 10:21:34 +0800

    补全EXCEL操作

---
+++ Excel格式解析/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace Excel格式解析
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.comboBox1);
+            this.groupBox3.Controls.Add(this.button12);
+            this.groupBox3.Controls.Add(this.button11);
+            this.groupBox3.Location = new System.Drawing.Point(14, 288);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Size = new System.Drawing.Size(521, 68);
+            this.groupBox3.TabIndex = 16;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "其他操作";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "HTML",
+            "CSV",
+            "TEXT",
+            "EXCEL",
+            "XML"});
+            this.comboBox1.Location = new System.Drawing.Point(204, 28);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(133, 23);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(357, 24);
+            this.button12.Margin = new System.Windows.Forms.Padding(4);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(141, 29);
+            this.button12.TabIndex = 2;
+            this.button12.Text = "导出文件";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(29, 24);
+            this.button11.Margin = new System.Windows.Forms.Padding(4);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(141, 29);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "结束Excel进程";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button10);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.button7);
+            this.groupBox2.Controls.Add(this.button8);
+            this.groupBox2.Location = new System.Drawing.Point(13, 136);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(523, 121);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Excel基本操作";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(31, 25);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 29);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "插入行操作";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(153, 25);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 29);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "删除行操作";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(279, 75);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 29);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "写入批注";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(153, 75);
+            this.button10.Margin = new System.Windows.Forms.Padding(4);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(100, 29);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "删除列操作";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(279, 25);
+            this.button6.Margin = new System.Windows.Forms.Padding(4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 29);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "合并单元格";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(400, 25);
+            this.button7.Margin = new System.Windows.Forms.Padding(4);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 29);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "复制行操作";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(31, 75);
+            this.button8.Margin = new System.Windows.Forms.Padding(4);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 29);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "插入列操作";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button9);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(523, 92);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "保存和结束";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 36);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "创建空的Excel";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(199, 36);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 29);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "另存为的Excel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(376, 36);
+            this.button9.Margin = new System.Windows.Forms.Padding(4);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(124, 29);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "写入多行";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 363);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ Excel格式解析/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Excel格式解析
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region 私有变量
+
+        private string _path;  //winForm路径
+
+        private ExcelHelper _helper;  //工具对象
+
+        #endregion
+
+        #region 初始化
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _path = System.Environment.CurrentDirectory + "\\测试目录\\"; //获取winForm应用程序下
+            _helper = new ExcelHelper();
+        }
+        #endregion
+
+        #region 私有方法
+
+        private void ShowOK()
+        {
+            MessageBox.Show("成功！");
+        }
+
+        private void OpenExcel(string filepath)
+        {
+            MessageBox.Show("已生成文件：" + filepath);
+            ExcelHelper.OpenExcel(filepath);
+        }
+
+        #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _helper = new ExcelHelper();
+            string filepath = _path + "空的Excel.xls";
+            _helper.OutputFilePath = filepath;
+            _helper.SaveAsFile();
+            OpenExcel(filepath);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string filepath = _path + "另存为的Excel.xls";
+            _helper = new ExcelHelper(_path + "..\\template.xls", filepath);
+            _helper.SetCellsBackColor(1, 1, 2, 7, ColorIndex.粉红);
+            _helper.SaveAsFile();
+            OpenExcel(filepath);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            string filepath = _path + "初始化数据.xls";
+            _helper = new ExcelHelper(_path + "..\\template.xls", filepath);
+            object[,] oSArray = new object[2, 7];
+            oSArray[0, 0] = "'001";
+            oSArray[0, 1] = "'001";
+            oSArray[0, 2] = "'001";
+            oSArray[0, 3] = "3";
+            oSArray[0, 4] = "1";
+            oSArray[0, 5] = "0.6";
+            oSArray[0, 6] = "1.8";
+            oSArray[1, 0] = "'001";
+            oSArray[1, 1] = "'002";
+            oSArray[1, 2] = "'002";
+            oSArray[1, 3] = "3";
+            oSArray[1, 4] = "2";
+            oSArray[1, 5] = "0.6";
+            oSArray[1, 6] = "3.6";
+
+            _helper.ArrayToExcel(oSArray, 3, 1);
+            _helper.SaveAsFile();
+            OpenExcel(filepath);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string filepath = _path + "删除行.xls";
+            _helper = new ExcelHelper(_path + "初始化数据.xls", filepath);
+            _helper.InsertRows(3, 2);
+            _helper.SaveAsFile();
+            OpenExcel(filepath);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string filepath = _path + "删除行.xls";
+            _helper = new ExcelHelper(_path + "初始化数据.xls", filepath);
+            _helper.DeleteRows(3, 1);
+            _helper.SaveAsFile();
+            OpenExcel(filepath);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string filepath = _path + "合并行.xls";
+            _helper = new ExcelHelper(_path + "初始化数据.xls", filepath);
+            _helper.MergeCells(3, 1, 3, 3, "001");
+            _helper.SaveAsFile();
+            OpenExcel(filepath);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string filepath = _path + "复制行.xls";
+            _helper = new ExcelHelper(_path + "初始化数据.xls", filepath);
+            _helper.CopyRows(3, 3); //将第三行复制三次
+            _helper.SaveAsFile();
+            OpenExcel(filepath);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string filepath = _path + "插入列.xls";
+            _helper = new ExcelHelper(_path + "初始化数据.xls", filepath);
+            _helper.InsertColumns(8, 2);
+            _helper.SetCells(2, 8, "备注1");
+            _helper.SetCells(2, 9, "备注2");
+            _helper.SaveAsFile();
+            OpenExcel(filepath);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            string filepath = _path + "删除列.xls";
+            _helper = new ExcelHelper(_path + "初始化数据.xls", filepath);
+            _helper.DeleteColumns(1, 3); //删除前三列
+            _helper.SaveAsFile();
+            OpenExcel(filepath);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string filepath = _path + "写入批注.xls";
+            _helper = new ExcelHelper(_path + "初始化数据.xls", filepath);
+            _helper.SetCellComment(2, 1, "goodsid");
+            _helper.SetCellComment(2, 2, "sku1id");
+            _helper.SetCellComment(2, 3, "sku2id");
+            _helper.SetCellComment(2, 4, "qty");
+            _helper.SaveAsFile();
+            OpenExcel(filepath);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            ExcelHelper.KillAllExcelProcess();
+            ShowOK();
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            string filetype = comboBox1.Text;
+            string extensions = string.Empty;
+            SaveAsFileFormat format = SaveAsFileFormat.EXCEL;
+            if (filetype == "")
+            {
+                MessageBox.Show("请选择一项！");
+                return;
+            }
+
+            switch (filetype)
+            {
+                case "EXCEL":
+                    extensions = "xls";
+                    format = SaveAsFileFormat.EXCEL;
+                    break;
+                case "CSV":
+                    extensions = "csv";
+                    format = SaveAsFileFormat.CSV;
+                    break;
+                case "TEXT":
+                    extensions = "txt";
+                    format = SaveAsFileFormat.TEXT;
+                    break;
+                case "XML":
+                    extensions = "xml";
+                    format = SaveAsFileFormat.XML;
+                    break;
+                case "HTML":
+                    extensions = "html";
+                    format = SaveAsFileFormat.HTML;
+                    break;
+            }
+            string filepath = _path + "导出文件." + extensions;
+
+            _helper = new ExcelHelper(_path + "初始化数据.xls", filepath);
+            _helper.SaveAsFile(format);
+            MessageBox.Show("已导出文件！");
+        }
+    }
+}
